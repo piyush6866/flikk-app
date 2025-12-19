aaa68fbd8976cae639a9fd3139162b914d0267ea
Piyush Ranjan <piyush.ranjan@planetspark.in>
2025-12-20 04:43:04 +0530

    fixing design ui preview

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def home
+    # Redirect logged-in users to their respective dashboards
+    if user_signed_in?
+      if current_user.brand?
+        redirect_to brand_dashboard_path
+      else
+        redirect_to creator_dashboard_path
+      end
+    end
   end
 end
 
